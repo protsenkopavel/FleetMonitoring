726c9388ec1d052b0d2b51c138d477063e600016
Pavel Protsenko <pavelprotsenko@outlook.com>
2025-03-11 00:15:03 +0300

    ui: rest template ручка для теста

--- UiService/src/main/java/net/protsenko/uiserivce/controller/CarSettingsController.java
+++ UiService/src/main/java/net/protsenko/uiserivce/controller/CarSettingsController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping("/api/v1/ui/")
 public class CarSettingsController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     private CarSettings carSettings = new CarSettings(10, 15.0, 20.0, 30.0);
 
@@ -23,6 +26,10 @@ public class CarSettingsController {
     @PostMapping("car-settings/")
     public String updateCarSettings(@ModelAttribute CarSettings updatedSettings) {
         this.carSettings = updatedSettings;
+
+        String dataGeneratorUrl = "http://localhost:8081/api/v1/data-generator/generate/";
+        restTemplate.postForLocation(dataGeneratorUrl, updatedSettings);
+
         return "redirect:/api/v1/ui/car-settings/";
     }
 
