7d4cfd216ac1524480b4dcad93da101cae6ee788
Pavel Protsenko <pavelprotsenko@outlook.com>
2025-03-10 22:55:15 +0300

    ui-service init

---
+++ UiService/src/main/java/net/protsenko/uiserivce/UiSerivceApplication.java
@@ -0,0 +1,13 @@
+package net.protsenko.uiserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiSerivceApplication.class, args);
+    }
+
+}
---
+++ UiService/src/test/java/net/protsenko/uiserivce/UiSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.uiserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
