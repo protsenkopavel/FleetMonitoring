d2b79bcffb45292de59ca58c680b2f674fa0b9c3
Pavel Protsenko <pavelprotsenko@outlook.com>
2025-03-12 22:04:35 +0300

    simulator: project init

---
+++ RealTimeSimulatorService/src/main/java/net/protsenko/realtimesimulatorservice/RealTimeSimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package net.protsenko.realtimesimulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeSimulatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeSimulatorServiceApplication.class, args);
+    }
+
+}
---
+++ RealTimeSimulatorService/src/test/java/net/protsenko/realtimesimulatorservice/RealTimeSimulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.realtimesimulatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeSimulatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
